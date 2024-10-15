010c62f089d8e456f8d1f632feb5049887357dc9
Matheus Valandro <msvalandro@proton.me>
2024-10-15 17:39:56 +0100

    chore: initial commit with project files

---
+++ app/src/main/java/com/msvalandro/musicappui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.msvalandro.musicappui
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.msvalandro.musicappui.ui.theme.MusicAppUITheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MusicAppUITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/msvalandro/musicappui/ui/theme/MainView.kt
@@ -0,0 +1,8 @@
+package com.msvalandro.musicappui.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainView() {
+    
+}
